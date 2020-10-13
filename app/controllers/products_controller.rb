bd85f91444c6dc331919aaedb18ea6a52fef7702
shtsukada <st01251988@gmail.com>
2020-10-13 13:04:50 +0900

    検索条件の追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     set_product_column
+    set_category_column
   end
 
   def search
@@ -19,6 +20,12 @@ class ProductsController < ApplicationController
 
   def set_product_column
     @product_name = Product.select("name").distinct #重複なくnameカラムのデータを取り出す
+    @product_size = Product.select("size").distinct
+    @product_status = Product.select("status").distinct
+  end
+
+  def set_category_column
+    @category_name = Category.select("name").distinct
   end
 
 end
